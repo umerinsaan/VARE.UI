9cdbda99d63b8c2c134c5d95ef624ad68f6b9811
umerimran <umer.imran@octdaily.com>
2025-05-12 03:39:25 +0500

    initial commit

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  imports: [MenuModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+}
